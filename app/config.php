6d8fd33c34a9e4ff8841598333f131c8577f617f
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2015-10-19 16:16:37 -0300

    Update config.php

--- app/config.php
+++ app/config.php
@@ -26,6 +26,7 @@
  */
 
 global $CFG;
+echo $CFG->dirroot . "/local/facebook/app/Facebook-php-sdk-v4/src/Facebook/autoload.php";
 require_once ($CFG->dirroot . "/local/facebook/app/Facebook-php-sdk-v4/src/Facebook/autoload.php");
 
 $AppID= $CFG->fbkAppID;
