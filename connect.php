0193c8b703060d8e46bc883cea4d5e50f6165c6f
Mauricio94 <mauricio94media@hotmail.com>
2015-11-06 13:12:13 -0300

    Arreglo para obtencion de AccessToken

--- connect.php
+++ connect.php
@@ -31,6 +31,7 @@ require_once (dirname ( dirname ( dirname ( __FILE__ ) ) ) . "/config.php");
 include "app/config.php";
 global $DB, $USER, $CFG;
 require_once ( $CFG->dirroot . "/local/facebook/forms.php" );
+use Facebook\Facebook;
 use Facebook\FacebookRedirectLoginHelper;
 use Facebook\Response;
 use Facebook\FacebookSDKException;
